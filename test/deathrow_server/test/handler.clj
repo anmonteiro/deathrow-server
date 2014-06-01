125f40a2238eab6637ead916c35243514bd577c1
António Nuno Monteiro <anmonteiro@gmail.com>
2014-06-01 04:15:57 +0100

    add more tests

--- test/deathrow_server/test/handler.clj
+++ test/deathrow_server/test/handler.clj
@@ -1,15 +1,41 @@
 (ns deathrow-server.test.handler
   (:use clojure.test
         ring.mock.request
-        deathrow-server.handler))
+        deathrow-server.handler
+        cheshire.core
+        somnium.congomongo))
 
 (deftest test-app
-  (testing "random route"
-    (let [response (app (request :get "/offenders/random"))]
-      (is (= (:status response) 200))
-      (is (= (re-matches #".*?application/json.*?" (get (:headers response) "Content-Type")) "application/json; charset=utf-8"))
-      (is (not= (re-find #"lastStmtUrl" (:body response)) nil))))
-  
+  (testing ""
+    (let [response (app (request :get "/offenders/414"))]
+      (is (= (re-matches #".*?application/json.*?" (get (:headers response) "Content-Type")) "application/json; charset=utf-8"))))
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
+(deftest test-offenders
+  (testing "random route"
+    (let [response (app (request :get "/offenders/random"))]
+      (is (= (:status response) 302))))
+
+  (testing "get all offenders"
+    (let [response (app (request :get "/offenders/"))]
+      (is (= (:status response) 200))
+      (let [body (parse-string (:body response) true)
+            total (count (:data body))]
+        (is (= total 20)))))
+
+  (testing "pagination"
+    (let [response (app (request :get "/offenders/page/4"))]
+      (let [body (parse-string (:body response) true)
+            total (count (:data body))]
+        (is (= total 20))
+        (is (= (count (:paging body)) 2)))))
+
+  (testing "get offender by execution number"
+    (let [response (app (request :get "/offenders/414"))]
+      (let [id (get (parse-string (:body response) true) :_id)]
+      (is (= id 999313)))))
+  (close-connection conn))
+  
