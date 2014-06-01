d70c6c835321fbe0d965e44705c29c3d060c8267
António Nuno Monteiro <anmonteiro@gmail.com>
2014-06-01 03:37:19 +0100

    implement get and paging

--- src/deathrow_server/handler.clj
+++ src/deathrow_server/handler.clj
@@ -11,15 +11,39 @@
 		[ring.adapter.jetty :as jetty])
 	(:gen-class))
 
+;; CONSTANTS
+(def PAGE-SIZE 20)
+
 (def conn
 	(mongo/make-connection "deathrow"
 		:host "127.0.0.1"
 		:port 27017))
 
+
+
 (defn get-all-offenders
+	"Return offenders using pagination. We don't
+	need to test for error in numbers, since our routing
+	already does that, e.g. o need for try/catch."
 	([] (get-all-offenders 1))
 	([page-num]
-		(response (str page-num))))
+		(let
+			[page-num (Integer. page-num)
+			offenders
+				(mongo/fetch
+					:offenders
+					:sort {
+						:executionNo -1
+					}
+					:skip (* (dec page-num) PAGE-SIZE)
+					:limit PAGE-SIZE)]
+			(response {
+				:data offenders
+				:paging {
+					:prev (dec page-num)
+					:next (inc page-num)
+				}
+			}))))
 
 
 (defn get-random-statement
