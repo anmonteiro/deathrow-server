a0776181d472cf24461b5f66dd3cf48da07aa76f
António Nuno Monteiro <anmonteiro@gmail.com>
2015-10-01 13:45:18 +0200

    bump deps, add travis config

--- project.clj
+++ project.clj
@@ -2,17 +2,16 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.8"]
-                 [congomongo "0.4.4"]
-                 [ring/ring-json "0.3.1"]
-                 [ring "1.2.1"]
-                 [ring-cors "0.1.2"]
-                 [ring/ring-jetty-adapter "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [congomongo "0.4.6"]
+                 [ring/ring-json "0.4.0"]
+                 [ring "1.4.0"]
+                 [ring-cors "0.1.7"]]
   :main deathrow-server.handler
-  :plugins [[lein-ring "0.8.10"]]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler deathrow-server.handler/app}
   :uberjar-name "deathrow-server-0.1.0-standalone.jar"
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring/ring-mock "0.3.0"]]}})
