9e02d2473b87f4b49d5492fa1c64c901e4d1879b
António Nuno Monteiro <anmonteiro@gmail.com>
2015-11-24 18:08:58 +0100

    add simple transit read helper to tests

--- test/deathrow_server/test/handler.clj
+++ test/deathrow_server/test/handler.clj
@@ -13,6 +13,15 @@
 
 (use-fixtures :once db-fixture)
 
+;; Helpers
+
+(defn- response-body->clj [res]
+  (t/read (t/reader (ByteArrayInputStream.
+                     (.getBytes (:body res)))
+                    :json)))
+
+;; Tests
+
 (deftest test-app
   (testing ""
     (let [response (app (mock/request :get "/offenders/414"))]
@@ -24,13 +33,9 @@
       (is (= (:status response) 404))))
   (testing "responses wrapped in :data"
     (let [response-o (app (mock/request :get "/offenders/414"))
-          body-o (t/read (t/reader (ByteArrayInputStream.
-                                    (.getBytes (:body response-o)))
-                                   :json))
+          body-o (response-body->clj response-o)
           response-os (app (mock/request :get "/offenders"))
-          body-os (t/read (t/reader (ByteArrayInputStream.
-                                     (.getBytes (:body response-os)))
-                                    :json))]
+          body-os (response-body->clj response-os)]
       (are [body] (contains? body :data)
         body-o
         body-os))))
@@ -42,23 +47,17 @@
   (testing "get all offenders"
     (let [response (app (mock/request :get "/offenders/"))]
       (is (= (:status response) 200))
-      (let [body (t/read (t/reader (ByteArrayInputStream.
-                                    (.getBytes (:body response)))
-                                   :json))
+      (let [body (response-body->clj response)
             total (count (:data body))]
         (is (= total 20)))))
   (testing "pagination"
     (let [response (app (mock/request :get "/offenders/page/4"))]
-      (let [body (t/read (t/reader (ByteArrayInputStream.
-                                    (.getBytes (:body response)))
-                                   :json))
+      (let [body (response-body->clj response)
             total (count (:data body))]
         (is (= total 20))
         (is (= (count (:paging body)) 2)))))
   (testing "get offender by execution number"
     (let [response (app (mock/request :get "/offenders/414"))]
-      (let [body (t/read (t/reader (ByteArrayInputStream.
-                                    (.getBytes (:body response)))
-                                   :json))
+      (let [body (response-body->clj response)
             id (get-in body [:data :_id])]
         (is (= id 999313))))))
